0404887d5f4e43bc961f1a7a1cfd07e5e80ad591
viy <pervak.s@gmail.com>
2013-04-01 00:22:02 +0300

    deployment setup

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -6,7 +6,7 @@ Refinery::Pages.configure do |config|
   # end
 
   # Configure global page default parts
-   config.default_parts = ["Banner", "Body"]
+   config.default_parts = ["Banner", "navigation", "Body", "Side Body"]
 
   # Configure whether to allow adding new page parts
    config.new_page_parts = true
