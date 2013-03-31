a4f33a3ab496349993a029f0bdb73714b207abac
viy <pervak.s@gmail.com>
2013-03-31 14:35:02 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sto2::Application.config.secret_token = '26e396f2070cdc578cdcad06402207ab1f87f9ff4fdc9cdd02e0b4c04ab2e943a221433839a47fc3b8512956899bd42e927ae9160d34e6bc9ff0c858fa84e20b'
